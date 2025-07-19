6d9674158a42b3d8e6e26378861ea0d3c039b801
MarkPheach <MTZeroH@gmail.com>
2025-07-19 17:19:10 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header class="flex items-center justify-between navbar bg-blue-500 text-white fixed top-0 left-0 w-full z-50 px-4 h-15">
+      <div class="flex-1 flex items-center">
+        <img src="../assets/kku_logo.svg" alt="Logo" class="h-10 w-auto mr-3" />
+        <h1 class="text-xl font-bold">
+            College of Computing, Subject Forum | ฟอรัมวิทยาลัยการคอมพิวเตอร์
+        </h1>
+      </div>
+
+      <div class="flex-none ml-auto mr-6">
+        <button class="btn btn-lg bg-white text-blue-500 border border-blue-500 rounded-lg px-6 py-2 hover:bg-gray-200">
+          Log out
+      </button>
+      </div>
+
+    </header>    
+    <main class="pt-15 px-4">
+      
+    </main>
+  </div>
+</template>
---
+++ pages/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Sidebar from '../components/Sidebar.vue';
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Sidebar />
+    <p>Welcome!</p>
+  </div>
+</template>
+
