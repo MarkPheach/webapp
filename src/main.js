d8a0cf6e2dd47856dc86f932efb6b334036d9060
Jaishoy <devjaishoy@gmail.com>
2025-09-11 13:55:19 +0700

    update webapp

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'       // ✅ import router ที่สร้างไว้
-import './style.css'
-
-createApp(App)
-  .use(router)                      // ✅ ใช้ router ก่อน mount
-  .mount('#app')
