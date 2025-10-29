a4f1ce19f4483269d7e661a25b9131f075cf1637
Panatpoom Norramut <panatpoom.n@kkumail.com>
2025-10-29 11:51:13 +0700

    icon overwrite

--- webapp-homepage/src/main.js
+++ webapp-homepage/src/main.js
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createPinia } from 'pinia' // ✅ import Pinia
-import router from './router'       // ✅ import router ที่สร้างไว้
+import { createPinia } from 'pinia'
+import router from './router'
+import vuetify from './plugins/vuetify/vuetify.js'
+
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'  // ✅ โหลด font MDI
 import './style.css'
 
 const pinia = createPinia()
 
 createApp(App)
-  .use(pinia)               // ✅ ใช้ Pinia
-  .use(router)                      // ✅ ใช้ router ก่อน mount
-  .mount('#app')  
+  .use(pinia)
+  .use(router)
+  .use(vuetify)  // ✅ ใช้ Vuetify
+  .mount('#app')
