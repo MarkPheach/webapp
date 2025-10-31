35ec7e1a5b145d1d71bcdf1d246bcf55d247fa25
Jaishoy <devjaishoy@gmail.com>
2025-10-31 20:20:37 +0700

    "รวมไฟล์คอมโพเนนต์และหน้าเว็บใหม่สำหรับหลักสูตร"

--- webapp-homepage/src/components/asideNav.vue
+++ webapp-homepage/src/components/asideNav.vue
@@ -2,7 +2,8 @@
 </script>
 
 <template>
-    <aside class="fixed left-0 min-h-[calc(100vh_-_10vh)] max-w-[calc(100vw_-_80vw)] bg-blue-300 flex flex-col items-center">
+    <aside
+        class="fixed left-0 min-h-[calc(100vh_-_10vh)] max-w-[calc(100vw_-_80vw)] bg-blue-300 flex flex-col items-center">
         <!-- user detail -->
         <div class="h-45 w-19/20 rounded-xl bg-white flex flex-col justify-center my-5 px-2">
             <h1 class="w-9/10">Name : UserName</h1>
@@ -11,20 +12,23 @@
             <h3 class="w-9/10">StudentDept : studentDepartment</h3>
         </div>
         <!-- page -->
-         <div class="h-55 w-19/20 flex flex-col my-5 px-2">
+        <div class="h-55 w-19/20 flex flex-col my-5 px-2">
             <h1 class="my-4 font-bold text-3xl"></h1>
             <ul>
                 <router-link to="/HomeInventory">
-                                     <li class="text-white text-3xl hover:text-5xl  transition duration-300"><button>Inventory</button></li>
+                    <li class="text-white text-3xl hover:text-5xl  transition duration-300"><button>Inventory</button>
+                    </li>
                 </router-link>
                 <router-link to="/HomeShop">
                     <li class="text-white text-3xl hover:text-5xl  transition duration-300"><button>Shop</button></li>
                 </router-link>
-                <li class="text-white text-3xl hover:text-5xl  transition duration-300"><button>Course</button></li>
+                <router-link to="/HomeCourse">
+                    <li class="text-white text-3xl hover:text-5xl  transition duration-300"><button>Course</button></li>
+                </router-link>
                 <router-link to="/HomePage">
-                                     <li class="text-white text-3xl hover:text-5xl  transition duration-300"><button>Home</button></li>
+                    <li class="text-white text-3xl hover:text-5xl  transition duration-300"><button>Home</button></li>
                 </router-link>
             </ul>
-         </div>
+        </div>
     </aside>
 </template>
--- webapp-homepage/src/router/index.js
+++ webapp-homepage/src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { useRouter } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import Login from '../pages/login.vue'
 import sigup from '../pages/signup.vue'
 import HomeShop from '../pages/HomeShop.vue'
+import HomeCourse from '../pages/HomeCourse.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
@@ -11,6 +11,7 @@ const routes = [
   { path: '/signup', name: 'Signup', component: sigup},
   { path: '/HomePage', name: 'HomePage', component: HomePage},
   { path: '/Homeshop', name: 'HomeShop', component: HomeShop},
+  { path: '/HomeCourse', name: 'HomeCourse', component: HomeCourse},
 ]
 
 const router = createRouter({
